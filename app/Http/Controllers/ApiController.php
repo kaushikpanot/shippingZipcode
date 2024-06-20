c60cbfc20a1269d59d0a1f280dead6b8dc8b5e81
Kaushik Panot <kaushik.panot@meetanshi.com>
2024-06-20 17:24:25 +0530

    Create Currency and Country list api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,9 +2,132 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use GuzzleHttp\Exception\RequestException;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Throwable;
 
 class ApiController extends Controller
 {
-    //
+    public function getCountryList(Request $request)
+    {
+        try {
+            // Retrieve the Shopify session
+            $shop = $request->attributes->get('shopifySession');
+            // $shop = "krishnalaravel-test.myshopify.com";
+
+            if (!$shop) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Token not provided.'
+                ], 400);
+            }
+
+            // Fetch the token for the shop
+            $token = User::where('name', $shop)->pluck('password')->first();
+
+            if (!$token) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'User not found.'
+                ], 404);
+            }
+
+            // Define the REST API endpoint
+            $restEndpoint = "https://{$shop}/admin/api/2024-04/countries.json";
+
+            // Headers for Shopify API request
+            $customHeaders = [
+                'X-Shopify-Access-Token' => $token,
+            ];
+
+            // Make HTTP GET request to Shopify REST API endpoint
+            $response = Http::withHeaders($customHeaders)->get($restEndpoint);
+
+            // Check if the request was successful
+            if ($response->successful()) {
+                // Decode the JSON response
+                $countriesList = $response->json('countries');
+
+                $collection = collect($countriesList);
+
+                $countries = $collection->map(function ($country) {
+                    unset($country['tax_name'], $country['tax'], $country['provinces']);
+                    return $country;
+                });
+
+                $responseData = [
+                    'countries' => $countries->all()
+                ];
+
+                return response()->json($responseData);
+            } else {
+                // Handle non-successful responses
+                return response()->json(['status' => false, 'error' => 'Unable to fetch country list']);
+            }
+        } catch (RequestException $e) {
+            // Handle request-specific exceptions
+            Log::error('HTTP request error', ['exception' => $e->getMessage()]);
+            return response()->json(['status' => false, 'message' => 'An unexpected error occurred:']);
+        } catch (Throwable $th) {
+            Log::error('Unexpected error', ['exception' => $th->getMessage()]);
+            return response()->json(['status' => false, 'message' => 'An unexpected error occurred:']);
+        }
+    }
+
+    public function getCurrencyList()
+    {
+        try {
+            // Retrieve the Shopify session
+            $shop = request()->attributes->get('shopifySession');
+
+            if (!$shop) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Token not provided.'
+                ], 400);
+            }
+
+            // Fetch the token for the shop
+            $token = User::where('name', $shop)->pluck('password')->first();
+
+            if (!$token) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'User not found.'
+                ], 404);
+            }
+
+            // Define the REST API endpoint
+            $restEndpoint = "https://{$shop}/admin/api/2024-04/currencies.json";
+
+            // Headers for Shopify API request
+            $customHeaders = [
+                'X-Shopify-Access-Token' => $token,
+            ];
+
+            // Make HTTP GET request to Shopify REST API endpoint
+            $response = Http::withHeaders($customHeaders)->get($restEndpoint);
+            // Check if the request was successful
+            if ($response->successful()) {
+                $responseData = [
+                    'currencies' => $response->json('currencies')
+                ];
+
+                return response()->json($responseData);
+            } else {
+                // Handle non-successful responses
+                return response()->json(['status' => false, 'error' => 'Unable to fetch Currency list']);
+            }
+        } catch (RequestException $e) {
+            // Handle request-specific exceptions
+            Log::error('HTTP request error', ['exception' => $e->getMessage()]);
+            return response()->json(['status' => false, 'message' => 'An unexpected error occurred:']);
+        } catch (Throwable $th) {
+            Log::error('Unexpected error', ['exception' => $th->getMessage()]);
+            return response()->json(['status' => false, 'message' => 'An unexpected error occurred:']);
+        }
+    }
 }
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class HomeController extends Controller
 {
     public function Index(Request $request){
         $post = $request->input();
         $shop = $request->input('shop');
-        
 
+         Log::info('App', $post);
         return view('welcome',compact('shop'));
     }
 

795cb643b89e4744cbce3c6b94f46ae476a4ca5e
Kaushik Panot <kaushik.panot@meetanshi.com>
2024-06-20 13:21:49 +0530

    React setup

---
+++ resources/js/Index.jsx
@@ -0,0 +1,22 @@
+import { AppProvider } from '@shopify/polaris';
+import React, { useState, useEffect } from 'react';
+import '@shopify/polaris/build/esm/styles.css';
+import '../../public/css/style.css';
+import translations from "@shopify/polaris/locales/en.json";
+import { BrowserRouter } from 'react-router-dom';
+import Main from './Pages/Main'
+import { Frame } from '@shopify/polaris';
+import { getSessionToken } from "@shopify/app-bridge-utils";
+
+export default function Index(props) {
+
+    return (
+        <BrowserRouter>
+            <AppProvider i18n={translations}>
+                <Frame>
+                    <Main {...props} />
+                </Frame>
+            </AppProvider>
+        </BrowserRouter>
+    );
+}
---
+++ resources/js/app.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useMemo, useState } from 'react';
+
+import Index from './Index';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('main');
+const props = Object.assign({}, container.dataset)
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+window.shopId = props.shopid;
+root.render(<Index tab="home"  {...props} />);

71c409e05c597914d579bca7db08248931c4c28b
Kaushik Panot <kaushik.panot@meetanshi.com>
2024-06-24 12:31:10 +0530

    Rate CRUD Operation APIs

--- routes/api.php
+++ routes/api.php
@@ -32,8 +32,8 @@ Route::middleware(['verify.shopify.jwt'])->group(function () {
         Route::delete('zone/{id}', 'zoneDestroy');
 
         Route::post('rate/save', 'rateStore');
-        // Route::get('rate/{name?}', 'zoneList');
-        // Route::get('zone/{id}/edit', 'zoneEdit');
-        // Route::delete('zone/{id}', 'zoneDestroy');
+        Route::get('rates/{zone_id}/{name?}', 'rateList');
+        Route::get('rate/{id}/edit', 'rateEdit');
+        Route::delete('rate/{id}', 'rateDestroy');
     });
 });

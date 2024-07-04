8ab7f47c9aa9df293a5a4446256eea0e889b8db1
Kaushik Panot <kaushik.panot@meetanshi.com>
2024-07-04 11:50:54 +0530

    Add mix rate column in setting table

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -9,7 +9,7 @@ class Setting extends Model
 {
     use HasFactory;
 
-    protected $fillable = ["user_id", "status", "shippingRate", "rateModifierTitle"];
+    protected $fillable = ["user_id", "status", "shippingRate", "rateModifierTitle", "mix_merge_rate", "mix_merge_rate_1", "additional_description_of_mix_rate", "max_price_of_auto_product_base_mix_rate"];
 
     protected $hidden = ['created_at', 'updated_at'];
 }

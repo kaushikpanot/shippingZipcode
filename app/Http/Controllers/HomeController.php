81de5f6f8eedad58004afc4f85c529e36333ac95
Kaushik Panot <kaushik.panot@meetanshi.com>
2024-09-02 15:39:00 +0530

    App Install/Uninstall mail set in tester mail

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -84,9 +84,7 @@ class HomeController extends Controller
         $token = User::where('name', $shopDetail)->first();
 
         $topics = [
-            'customers/update',
-            'customers/delete'
-            // 'shop/update'
+            'customers/update'
         ];
 
         $apiVersion = config('services.shopify.api_version');
@@ -119,58 +117,6 @@ class HomeController extends Controller
         }
         return true;
     }
-
-    // private function getStoreOwnerEmail($shop)
-    // {
-    //     $user = User::where('name', $shop)->pluck('password')->first();
-    //     $apiVersion = config('services.shopify.api_version');
-    //     $shop_url = "https://{$shop}/admin/api/{$apiVersion}/shop.json";
-
-    //     $curl = curl_init();
-    //     curl_setopt_array($curl, [
-    //         CURLOPT_URL => $shop_url,
-    //         CURLOPT_RETURNTRANSFER => true,
-    //         CURLOPT_ENCODING => "",
-    //         CURLOPT_MAXREDIRS => 10,
-    //         CURLOPT_TIMEOUT => 30,
-    //         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    //         CURLOPT_CUSTOMREQUEST => "GET",
-    //         CURLOPT_HTTPHEADER => [
-    //             "Content-Type: application/json",
-    //             "X-Shopify-Access-Token:" . $user
-    //         ],
-    //     ]);
-
-    //     $response = curl_exec($curl);
-    //     $err = curl_error($curl);
-
-    //     curl_close($curl);
-
-    //     if ($err) {
-    //         return false;
-    //     } else {
-    //         $data = json_decode($response, true);
-    //         // Log::info('input logs:', ['shopDetail' => $data]);
-    //         if (@$data['shop']) {
-    //             $storeOwnerEmail = "kaushik.panot@meetanshi.com";
-    //             // $storeOwnerEmail = $data['shop']['email'];
-    //             $store_name = $data['shop']['name'];
-    //             // User::where('name', $shop)->update(['store_owner_email' => $storeOwnerEmail, 'store_name' => $store_name]);
-    //             $details = [
-    //                 'title' => 'Thank You for Installing AI Content Generator for Shopify - Meetanshi',
-    //                 'name' => $store_name
-    //             ];
-
-    //             $name = $data['shop']['shop_owner'];
-    //             $shopDomain = $data['shop']['domain'];
-
-    //             Mail::to($storeOwnerEmail)->send(new InstallMail($name, $shopDomain));
-    //             Mail::to("panotkaushik@gmail.com")->send(new InstallMail($name, $shopDomain));
-
-    //             return true;
-    //         }
-    //     }
-    // }
 
     private function getStoreOwnerEmail($shop)
     {
@@ -221,16 +167,8 @@ class HomeController extends Controller
             //     'store_name' => $storeName,
             // ]);
 
-            // Prepare email details
-            $details = [
-                'title' => 'Thank You for Installing AI Content Generator for Shopify - Meetanshi',
-                'name' => $storeName,
-            ];
-
             // Send emails
-            Mail::to($storeOwnerEmail)->send(new InstallMail($shopOwner, $shopDomain));
-
-            Mail::to("krishna.patel@meetanshi.com")->send(new InstallSupportMail($shopOwner, $shopDomain));
+            Mail::to("bhushan.trivedi@meetanshi.com")->send(new InstallMail($shopOwner, $shopDomain));
 
             return true;
         } catch (\Exception $e) {

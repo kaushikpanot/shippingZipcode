4185014ca7f955d0075cf79498ca2ba051c9ec6c
Kaushik Panot <kaushik.panot@meetanshi.com>
2024-11-27 14:17:27 +0530

    Change route path

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,7 @@ Route::get('recurring/create', [RecurringChargeController::class, 'createRecurri
 Route::get('recurring/confirm', [RecurringChargeController::class, 'confirmRecurringCharge']);
 
 Route::get('/', [HomeController::class, 'index'])->middleware(['verify.shop', 'verify.shopify'])->name('home');
-Route::get('/{path}', [HomeController::class, 'common'])->middleware(['verify.shop']);
-
-    // Route::get('/{path?}', [HomeController::class, 'common'])
-    // ->where('path', '^(?!uploads).')
-    // ->where('path', '..(?!jpg|jpeg|png|gif|bmp|ico|webp).')
-    // ->where('path', '.*')
-    // ->fallback();
+Route::get('/{path}', [HomeController::class, 'common'])->where('path', '[a-zA-Z0-9-_]+')->middleware(['verify.shop']);
 
 
 Route::post('/webhook/checkouts-create', [WebhookController::class, 'handleCheckoutsCreate']);
